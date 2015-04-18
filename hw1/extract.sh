504f81c02f64544b510aa62d59ab15625a40df38
Kenley Arai <me@kenleyarai.com>
2015-04-18 10:58:06 -0700

    Added additional functionality to extract.sh

--- hw1/extract.sh
+++ hw1/extract.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+rm ./*.txt
+
 for filename in *
 do
   new_dir=$(echo $filename | grep '[^_][A-Za-z]*[^_][0-9]\{3\}' -o)
-  mkdir -p $new_dir/build
-  tar -zxvf "$filename" -C $new_dir
-  mv ./*$new_dir* $new_dir
+
+  if [[ $filename =~ \.tar$  ]];
+  then
+    mkdir -p $new_dir/build
+    tar -xvf "$filename" -C $new_dir
+  elif [[ $filename =~ \.tar.gz$  ]];
+  then
+    mkdir -p $new_dir/build
+    tar -zxvf "$filename" -C $new_dir
+  elif [[ $filename =~ \.h|.H$  ]];
+  then
+    mkdir -p $new_dir/build
+    mv $filename $new_dir/MyList.hpp
+  else
+    echo "****ERROR*****"
+    echo $filename
+    echo "did not match any extension"
+  fi
 done
 
 #Fixes class getter
--- hw1/tests_MyList.py
+++ hw1/tests_MyList.py
@@ -1,9 +1,10 @@
 from MyList import MyList
+import re
 
-def test_print(capsys):
+def test_print(capfd):
     test_output = MyList("testoutput")
     test_output.print_list()
-    out = capsys.readout()
-    only_chars = "".join(re.findall('([a-z]+|[A-Z]+)', out))
+    out, err = capfd.readouterr()
+    only_chars = "".join(re.findall('([a-z])', out))
     assert only_chars == "testoutput"
 
