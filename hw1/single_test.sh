4c8001ae6740b741147301124542f570542d0292
Kenley Arai <me@kenleyarai.com>
2015-04-18 11:59:13 -0700

    1. Fixed bug with extraction script. 2. Added functionality to test a single student

---
+++ hw1/single_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$#" -eq 0 ] || [ "$#" -gt 1 ]
+then
+  echo "This script requires a single argument"
+elif [ -f $1 ]
+then
+  echo "This script takes only a directory as an argument"
+fi
+
+cp cs14_grading/hw1/tests_MyList.py $1/build
+cp cs14_grading/hw1/{CmakeLists.txt,MyList.cpp} $1
+
+cd $1/build
+cmake -DPYTHON_LIBRARY=/usr/local/Cellar/python/2.7.9/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib ../
+make
+py.test -v tests_MyList.py > tests.log
