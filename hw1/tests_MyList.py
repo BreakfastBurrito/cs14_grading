334f5bb53fff35b515b8f3419d4211dd6779e89f
Antoine Guerrero <asguerrero3@gmail.com>
2015-04-20 11:56:02 -0700

    Added a few tests that need to be completed

--- hw1/tests_MyList.py
+++ hw1/tests_MyList.py
@@ -1,7 +1,47 @@
 from MyList import MyList
 import re
 
-def test_print(capfd):
+# default constructor
+def test_default_constructor():
+
+# copy constructor
+def test_copy_constructor():
+
+# Test string constructor
+def test_string_constructor():
+
+# Test char* constructor
+def test_char_constructor():
+
+# destructor
+def test_destructor():
+
+# push_front on empty
+def test_push_front_empty():
+
+# push_front on nonempty
+def test_push_front_nonempty():
+
+# push_back on empty
+def test_push_back_empty():
+
+# push_back on empty
+def test_push_back_nonempty():
+
+# pop_front on empty
+def test_pop_front_empty():
+
+# pop_front on empty
+def test_pop_front_nonempty():
+
+# pop_back empty
+def test_pop_back_empty():
+
+# pop_back empty
+def test_pop_back_empty():
+
+# print
+def test_print(capsys):
     test_output = MyList("testoutput")
     test_output.print_list()
     out, err = capfd.readouterr()
